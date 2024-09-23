f903ef60889c121c2a9fe8c8f3082337a9b4a0ab
wynify <95828546+wynify@users.noreply.github.com>
2024-09-23 14:37:51 +0000

    pack everything

---
+++ sdl_latex/main.cpp
@@ -0,0 +1,66 @@
+/*Not in use
+
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+void generatePDF() {
+    std::ofstream file("document.tex");
+    file << "\\documentclass{article}\n";
+    file << "\\begin{document}\n";
+    file << "Hello, this is a simple LaTeX document generated from C++!\n";
+    file << "\\end{document}\n";
+    file.close();
+
+    system("pdflatex document.tex");
+}
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL and LaTeX Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    bool quit = false;
+    SDL_Event e;
+
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    generatePDF();
+
+    std::cout << "PDF document generated as document.pdf" << std::endl;
+
+    return 0;
+}
+*/
